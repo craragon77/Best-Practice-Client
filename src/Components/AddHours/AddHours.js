e591b5f9ab5a42ebee49578d2d8e2560332fb5d4
Chris Aragon <aragonchristopherray@gmail.com>
2020-08-28 10:55:54 -0400

    i forget if I saved this

--- src/Components/AddHours/AddHours.js
+++ src/Components/AddHours/AddHours.js
@@ -1,9 +1,30 @@
 import React, {Component} from 'react';
+import MUSIC from '../Dummy-Music/Dummy-Music';
 
 export default class AddHours extends Component{
+    handleSubmit = (e) => {
+        e.preventDefault();
+        alert('Click successful!')
+    }
     render(){
+        const songOptions = MUSIC.MUSIC.map(i => 
+            <option value={i.title + 'by' + i.composer}>{i.title + ' by ' + i.composer}</option>
+        )
         return(
-            <p>There is no pepe Silvia!</p>
+            <>
+                <h1>Log Practice Hours</h1>
+                <form>
+                    <label for="piece">Piece Rehersed</label><br/>
+                    <select name="songs">
+                        {songOptions}
+                    </select><br/>
+                    <label for="hours">How long did you practice?</label><br/>
+                    <input type="number" name="hours"/><br/>
+                    <label for="date">Date Practiced?</label><br/>
+                    <input type="date" name="date"></input><br/>
+                    <button onClick={this.handleSubmit}>Submit</button>
+                </form>
+            </>
         )
     }
 }
