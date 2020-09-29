a4617c6c6ebbea5ab594e3d1b272ad17c0482d3b
Chris Aragon <aragonchristopherray@gmail.com>
2020-09-29 13:16:02 -0400

    first commit of style branch

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,7 +7,8 @@ export default class Header extends Component{
     constructor(props){
         super(props);
             this.state = {
-                hasAuthToken: TokenService.hasAuthToken()
+                hasAuthToken: TokenService.hasAuthToken(),
+                menuOpen: false
             }
     }
 
@@ -42,7 +43,43 @@ export default class Header extends Component{
         )
     }
 
+    handleMenuClick() {
+        this.setState({menuOpen:!this.state.menuOpen});
+    }
+    
+    handleLinkClick() {
+        this.setState({menuOpen: false});
+    }
+
     render(){
+        const styles= 
+        {
+          container:{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            zIndex: '99',
+            opacity: 0.9,
+            display:'flex',
+            alignItems:'center',
+            background: 'black',
+            width: '100%',
+            color: 'white',
+            fontFamily:'Lobster',
+          },
+          logo: {
+            margin: '0 auto',
+          },
+          body: {
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            width: '100vw',
+            height: '100vh',
+            filter: this.state.menuOpen ? 'blur(2px)':null,
+            transition: 'filter 0.5s ease',
+          },
+        }
         return(
             <div className="Header-Links">
                 <Link to="/">Home</Link>
